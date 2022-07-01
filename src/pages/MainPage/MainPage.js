cdc73de2edf5c1b26d6e187eefabfad511db57f9
Sadegh <mo.ma4115@gmail.com>
2022-07-01 21:28:08 +0430

    connect tto wallet 2

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -6,19 +6,22 @@ import {useState} from "react";
 function MainPage() {
 
     const [walletState,setWalletState] = useState(false)
+    const [relationType,setRelationType] = useState()
 
     return (
        <>
             <div className="sideBarContainer">
                 <img className={"p-4 text-light rounded-circle"} src={authorProfile}/>
                 <span className={"text-light fw-bold "}>Philipe inzagi</span>
-                <Button className={"m-2"} variant={"outlined"}>{!walletState ? "Connect wallet":"wallet connected"}</Button>
-                <Button className={"m-2"} variant={"outlined"}>Edit Profile</Button>
+                <Button className={"m-2 text-light border-light generalBtn"}
+                        variant={"contained"}>{!walletState ? "Connect wallet":"wallet connected"}</Button>
+                <Button className={"m-2  text-light border-light"} variant={"outlined"}>Edit Profile</Button>
                 <div className={"d-flex flex-column justify-content-end flex-grow-1 "}>
-                    <Button className={"m-2 mb-3"} variant={"outlined"}> log out</Button>
+                    <Button className={"m-2 mb-3 text-light border-light"} variant={"outlined"}> log out</Button>
                 </div>
             </div>
             <div className={"mainContainer"}>
+
 
 
             </div>
