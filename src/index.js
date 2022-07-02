33b958866e615444bdaf5a5816d3348c9de1de8a
Mohammad Akrami <m.akramifard@gmail.com>
2022-07-02 15:00:27 +0430

    creating wallet

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+window.Buffer = "sdklm";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );
