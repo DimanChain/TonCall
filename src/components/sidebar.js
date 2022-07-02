d5b04979663d013134a4873da21132b4d2ab9bf7
Mohammad Akrami <m.akramifard@gmail.com>
2022-07-02 18:46:36 +0430

    useWeb3

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,42 +1,45 @@
 import authorProfile from '../assets/profile-author.jpg'
-import {Button, Box, Modal} from '@mui/material'
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState,} from "react";
+import { Button, Box, Modal, TextField } from '@mui/material'
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState, } from "react";
 import * as tonMnemonic from "tonweb-mnemonic";
 import classes from "./sidebar.module.css";
 import TonWeb from 'tonweb';
 import { setUserInfo } from "../redux/AppInfoSlice";
+import useWeb3 from "../hooks/useWeb3";
 var mnemonicWords;
 
 function Sidebar() {
     const [open, setOpen] = useState(false);
+    const [depositAmount, setDepositAmount] = useState(0);
 
     const [walletState, setWalletState] = useState(0)
     const [modalState, setModalState] = useState(0)
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    const dispatch = useDispatch();
-    const {publicKey, secretKey, walletAddress, balance} = useSelector((s) => s.appInfo);
+    const { getBalance } = useWeb3();
 
-    const walletConnect = async (event) => {
+    const dispatch = useDispatch();
+    const { publicKey, secretKey, walletAddress, balance } = useSelector((s) => s.appInfo);
+
+    const deposite = async (event) => {
         try {
             console.log(window.ton)
             if (!window.ton)
                 return window.open('https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd', "_blank");
-            let resp = await window.ton.send("ton_requestWallets")
-            localStorage.setItem("publicKey", resp[0].publicKey);
+            await window.ton.send("ton_requestWallets")
+            console.error(depositAmount)
+            await window.ton.send("ton_sendTransaction", [
+                { value: depositAmount, to: walletAddress, data: "From TonCall" },
+            ])
+            setTimeout(function () {
+                window.location.reload()
 
-            window.ton
-                .send("ton_rawSign", [
-                    {data: "Welcome to TonCall this is just a simple test for signing your wallet"},
-                ], () => {
-
-                });
+            }, 1000);
         } catch (error) {
             console.log(error);
         }
-
 
     }
 
@@ -51,28 +54,23 @@ function Sidebar() {
     //----------------------------------------------------------------
     async function confirmWallet() {
         await tonMnemonic.validateMnemonic(mnemonicWords);
-        // -> true
-
         await tonMnemonic.isPasswordNeeded(mnemonicWords);
-        // -> false
-
         await tonMnemonic.mnemonicToSeed(mnemonicWords);
-        // -> Uint8Array(32) [183, 90, 187, 181, .. ]
 
         const keyPair = await tonMnemonic.mnemonicToKeyPair(mnemonicWords);
-        // -> {publicKey: Uint8Array(32), secretKey: Uint8Array(64)}
+
+        localStorage.setItem("publicKey", toHexString(keyPair.publicKey));
+        localStorage.setItem("secretKey", toHexString(keyPair.secretKey));
+        const tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', { apiKey: '9d3e42ecad4110e05084be39b30cd6e1e30ea7c67777a8c2c434ba3fa29cd0b1' }));
+        const wallet = await tonweb.wallet.create({ publicKey: keyPair.publicKey });
+        localStorage.setItem("walletAddress", (await wallet.getAddress()).toString(true, true, true));
+
         dispatch(setUserInfo({
             publicKey: toHexString(keyPair.publicKey),
             secretKey: toHexString(keyPair.secretKey),
-            walletAddress: toHexString((await wallet.getAddress()).hashPart),
-            balance : 10
+            walletAddress: (await wallet.getAddress()).toString(true, true, true),
+            balance: await tonweb.getBalance((await wallet.getAddress()).toString(true, true, true))
         }));
-        localStorage.setItem("publicKey", toHexString(keyPair.publicKey));
-        localStorage.setItem("secretKey", toHexString(keyPair.secretKey));
-        const tonweb = new TonWeb();
-        const wallet = await tonweb.wallet.create({publicKey: keyPair.publicKey});
-        localStorage.setItem("walletAddress", toHexString((await wallet.getAddress()).hashPart));
-
         handleClose();
     }
 
@@ -106,7 +104,7 @@ function Sidebar() {
                 </p>
                 <div>
                     <Button className={"m-2 text-light border-light generalBtn"} onClick={confirmWallet}
-                            variant={"contained"}>Confirm and Create Wallet</Button>
+                        variant={"contained"}>Confirm and Create Wallet</Button>
                 </div>
             </div>)
     }
@@ -115,15 +113,32 @@ function Sidebar() {
         localStorage.removeItem("publicKey");
         localStorage.removeItem("secretKey");
         localStorage.removeItem("walletAddress");
+        dispatch(setUserInfo({
+            publicKey: "",
+            secretKey: "",
+            walletAddress: "",
+            balance: 10
+        }));
         setModalState(0);
     }
 
     useEffect(() => {
-        setTimeout(() => {
+        setTimeout(async () => {
             if (!window.ton)
                 setWalletState(2);
             else
                 setWalletState(0);
+
+            const tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', { apiKey: '9d3e42ecad4110e05084be39b30cd6e1e30ea7c67777a8c2c434ba3fa29cd0b1' }));
+            if (localStorage.getItem("secretKey")) {
+                dispatch(setUserInfo({
+                    publicKey: localStorage.getItem("publicKey"),
+                    secretKey: localStorage.getItem("secretKey"),
+                    walletAddress: localStorage.getItem("walletAddress"),
+                    balance: await tonweb.getBalance(localStorage.getItem("walletAddress"))
+                }));
+
+            }
         }, 100)
     }, []);
 
@@ -132,22 +147,29 @@ function Sidebar() {
             <div className={classes.sideBarContainer}>
 
                 {
-                    localStorage.getItem("secretKey") ?
+                    secretKey && secretKey != "" ?
                         <>
-                            <img className={"p-4 text-light rounded-circle"} src={authorProfile}/>
+                            <img className={"p-4 text-light rounded-circle"} src={authorProfile} />
                             <a onClick={() => {
                                 navigator.clipboard.writeText(walletAddress)
                             }}
-                               className={"text-light fw-bold "}>{walletAddress.substring(0, 4) + "...." + walletAddress.substring(walletAddress.length - 4, walletAddress.length)}</a>
-                            <Button className={"m-2 text-light border-light generalBtn"} onClick={walletConnect}
-                                    variant={"contained"}>{walletState == 0 ? "Connect wallet" : walletState == 1 ? "Wallet Connected" : "Install Ton Wallet"}</Button>
+                                className={"text-light fw-bold "}>{walletAddress.substring(0, 4) + "...." + walletAddress.substring(walletAddress.length - 4, walletAddress.length)}</a>
+                            <div className='m-2 text-white'>
+                                <div>
+                                    TON Balance:
+                                </div>
+                                <div>
+                                    {TonWeb.utils.fromNano(balance) + " 💎 "}
+                                </div>
+
+                            </div>
                             <Button className={"m-2 text-light border-light"} variant={"outlined"}
-                                    onClick={handleOpen}>Deposite</Button>
+                                onClick={handleOpen}>Deposite</Button>
                         </> :
                         <div className={"m-2 mt-5 text-light border-light"}>
                             <span className={"text-light fw-bold "}>To use our platform and connect to other user you have to create a wallet or import one</span>
                             <Button className={"m-2 text-light border-light generalBtn"} onClick={createWallet}
-                                    variant={"contained"}>Create Wallet</Button>
+                                variant={"contained"}>Create Wallet</Button>
                             <Button className={"m-2 text-light border-light"} variant={"outlined"} onClick={handleOpen}>Import
                                 Wallet</Button>
                         </div>
@@ -168,11 +190,25 @@ function Sidebar() {
                 <Box className={classes.modalStyle}>
                     {
                         modalState == 1 ? createWalletBody() :
-                            <>
-                                <div>HI</div>
-                                <Button className={"m-2 text-light border-light generalBtn"} onClick={createWallet}
-                                        variant={"contained"}>Create Wallet</Button>
-                            </>
+                            <div className='text-center'>
+                                <div>Send TON 💎 to this address: </div>
+                                <div className='mt-3 mb-3'>{walletAddress}</div>
+                                <div className='text-warning mb-2'>OR</div>
+                                <div className="row g-3 justify-content-center align-items-center">
+
+                                    <div className="col-auto">
+                                        <input type="number" onChange={(e) => {
+                                            console.log(e.target.value * 10 ** 9);
+                                            setDepositAmount(e.target.value * 10 ** 9)
+                                        }} id="inputPassword6" className="form-control" aria-describedby="passwordHelpInline" placeholder="Enter Amout " />
+                                    </div>
+
+                                </div>
+                                <Button className={"m-2 text-light border-light generalBtn"} onClick={deposite}
+                                    variant={"contained"}>DOPSITE FROM TON WALLET</Button>
+                                <Button className={"m-2 text-light border-light generalBtn"} onClick={handleClose}
+                                    variant={"contained"} color={"error"}>Cancel</Button>
+                            </div>
                     }
 
                 </Box>
--- src/hooks/useWeb3.js
+++ src/hooks/useWeb3.js
@@ -1,11 +1,23 @@
+import TonWeb from 'tonweb';
+
+
+
+const BN = TonWeb.utils.BN;
+const toNano = TonWeb.utils.toNano;
+
+const providerUrl = 'https://testnet.toncenter.com/api/v2/jsonRPC';
+const apiKey = '9d3e42ecad4110e05084be39b30cd6e1e30ea7c67777a8c2c434ba3fa29cd0b1';
+const tonweb = new TonWeb(new TonWeb.HttpProvider(providerUrl, { apiKey }));
 
 const useWeb3 = () => {
 
-    const getBalance = async (publicKey) => {
-        //let balance = await ...
+    const getBalance = async (walletAddress) => {
+
     };
 
-    return { getBalance};
+
+
+    return { getBalance };
 };
 
 export default useWeb3;
