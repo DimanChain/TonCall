d123e1a42be79dc0073f2091dfef7fd5e1c1bb82
Mohammad Akrami <m.akramifard@gmail.com>
2022-07-02 15:00:19 +0430

    creating wallet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
 import { Routes, Route, Outlet } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import ChatPage from "./pages/ChatPage/ChatPage";
+import { Buffer } from 'buffer'
+
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<MainPage />}/>
-                <Route path="/connect/:categoryId" element={<ChatPage />}/>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/connect/:categoryId" element={<ChatPage />} />
             </Routes>
             <Outlet />
         </div>
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,16 +2,17 @@
 import authorProfile from '../assets/profile-author.jpg'
 import { Button, Box, Modal } from '@mui/material'
 import { useEffect, useState } from "react";
+import * as tonMnemonic from "tonweb-mnemonic";
+import classes from "./sidebar.module.css";
 
-
-
-
-
+import useTonWeb from './useTonWeb';
+var mnemonicWords;
 
 function Sidebar() {
     const [open, setOpen] = useState(false);
 
     const [walletState, setWalletState] = useState(0)
+    const [modalState, setModalState] = useState(0)
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
@@ -37,12 +38,81 @@ function Sidebar() {
     }
 
 
+
+    //----------------------------------------------------------------
+    async function createWallet() {
+        mnemonicWords = await tonMnemonic.generateMnemonic();
+        setModalState(1);
+        handleOpen();
+    }
+    //----------------------------------------------------------------
+    async function confirmWallet() {
+        await tonMnemonic.validateMnemonic(mnemonicWords);
+        // -> true
+
+        await tonMnemonic.isPasswordNeeded(mnemonicWords);
+        // -> false
+
+        await tonMnemonic.mnemonicToSeed(mnemonicWords);
+        // -> Uint8Array(32) [183, 90, 187, 181, .. ]
+
+        const keyPair = await tonMnemonic.mnemonicToKeyPair(mnemonicWords);
+        // -> {publicKey: Uint8Array(32), secretKey: Uint8Array(64)}
+        localStorage.setItem("publicKey", toHexString(keyPair.publicKey));
+        localStorage.setItem("secretKey", toHexString(keyPair.secretKey));
+        // const tonweb = new TonWeb();
+
+        // const wallet = tonweb.wallet.create({ publicKey: toHexString(keyPair.publicKey) });
+        // localStorage.setItem("walletAddress", wallet.getAddress());
+
+        // console.log(wallet.getAddress());
+        handleClose();
+    }
+    function toHexString(byteArray) {
+        return Array.prototype.map.call(byteArray, function (byte) {
+            return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+        }).join('');
+    }
+
+
+    function createWalletBody() {
+        let resp = [];
+        for (let i = 0; i < mnemonicWords.length; i++) {
+            resp.push(
+                <>
+                    <span className="text-danger">
+                        {(i + 1) + "."}
+                    </span>
+                    <span>
+                        {mnemonicWords[i] + " "}
+                    </span>
+                </>
+            );
+
+
+        }
+        return (
+            <div>
+                {resp}
+                <p className='fw-bold mt-1'>
+                    Write down your 24 Words and keep it safe
+                </p>
+                <div>
+                    <Button className={"m-2 text-light border-light generalBtn"} onClick={confirmWallet}
+                        variant={"contained"}>Confirm and Create Wallet</Button>
+                </div>
+            </div>)
+    }
+
+
+
     useEffect(() => {
         setTimeout(() => {
             if (!window.ton)
                 setWalletState(2);
             else
                 setWalletState(0);
+
         }, 100)
     }, []);
 
@@ -53,16 +123,16 @@ function Sidebar() {
 
                 {
                     localStorage.getItem("publicKey") ?
-                        <div>
+                        <>
                             <img className={"p-4 text-light rounded-circle"} src={authorProfile} />
-                            <span className={"text-light fw-bold "}>Philipe inzagi</span>
+                            <span className={"text-light fw-bold "}>{localStorage.getItem("publicKey").substring(4) + "...." + localStorage.getItem("publicKey").substring(localStorage.getItem("publicKey").length - 4, 4)}</span>
                             <Button className={"m-2 text-light border-light generalBtn"} onClick={walletConnect}
                                 variant={"contained"}>{walletState == 0 ? "Connect wallet" : walletState == 1 ? "Wallet Connected" : "Install Ton Wallet"}</Button>
                             <Button className={"m-2 text-light border-light"} variant={"outlined"} onClick={handleOpen}>Deposite</Button>
-                        </div> :
+                        </> :
                         <div className={"m-2 mt-5 text-light border-light"}>
                             <span className={"text-light fw-bold "}>To use our platform and connect to other user you have to create a wallet or import one</span>
-                            <Button className={"m-2 text-light border-light generalBtn"} onClick={walletConnect}
+                            <Button className={"m-2 text-light border-light generalBtn"} onClick={createWallet}
                                 variant={"contained"}>Create Wallet</Button>
                             <Button className={"m-2 text-light border-light"} variant={"outlined"} onClick={handleOpen}>Import Wallet</Button>
                         </div>
@@ -78,8 +148,16 @@ function Sidebar() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box className={"modalStyle"}>
-                    <span>HI</span>
+                <Box className={classes.modalStyle}>
+                    {
+                        modalState == 1 ? createWalletBody() :
+                            <>
+                                <div>HI</div>
+                                <Button className={"m-2 text-light border-light generalBtn"} onClick={createWallet}
+                                    variant={"contained"}>Create Wallet</Button>
+                            </>
+                    }
+
                 </Box>
             </Modal>
         </>
